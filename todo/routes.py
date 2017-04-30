4d735368d99309c6c6fd35b9fb359868349da2b0
Chaker Benhamed <chaker.benhamed@predictix.com>
2017-04-30 21:56:49 +0100

    Implement first route

--- todo/routes.py
+++ todo/routes.py
@@ -1,2 +1,11 @@
+from pyramid.view import view_config
+from pyramid.response import Response
+
+
+@view_config(route_name='home')
+def home(request):
+    return Response('Hello World')
+
+
 def includeme(config):
-    pass
+    config.add_route('home', '/')
